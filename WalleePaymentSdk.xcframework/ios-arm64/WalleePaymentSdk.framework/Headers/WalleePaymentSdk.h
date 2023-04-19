aad28ab143f642daf1b9b52a9c06eac42877e4b6
wallee-deployment-user <technical-systems@wallee.com>
2023-04-19 09:08:54 +0200

    Release version: 1.0.0

---
+++ WalleePaymentSdk.xcframework/ios-arm64/WalleePaymentSdk.framework/Headers/WalleePaymentSdk.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+
+//! Project version number for WalleePaymentSdk.
+FOUNDATION_EXPORT double WalleePaymentSdkVersionNumber;
+
+//! Project version string for WalleePaymentSdk.
+FOUNDATION_EXPORT const unsigned char WalleePaymentSdkVersionString[];
+
+
+// In this header, you should import all the public headers of your framework using statements like #import <WalleePaymentSdk/PublicHeader.h>
